b08ce5a16293eddc5f5117aefdf21720d037d904
kycasdzxc <105793579+kycasdzxc@users.noreply.github.com>
2023-04-08 22:09:32 +0900

    프로젝트 변경 : Spring -> Boot

---
+++ src/main/java/RiddleGame/PilgrimProgress/PilgrimProgressApplication.java
@@ -0,0 +1,13 @@
+package RiddleGame.PilgrimProgress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PilgrimProgressApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PilgrimProgressApplication.class, args);
+	}
+
+}
---
+++ src/test/java/RiddleGame/PilgrimProgress/PilgrimProgressApplicationTests.java
@@ -0,0 +1,13 @@
+package RiddleGame.PilgrimProgress;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PilgrimProgressApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
